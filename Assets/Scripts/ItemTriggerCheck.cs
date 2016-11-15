07a92ed36895e515db8e16d54b20eb7f959509ae
Alexander Lövberg <lovbergalexander@gmail.com>
2016-11-15 18:54:05 +0100

    Item placement check and Scene changer for storyboard
    Temporary scripts that needs to be worked on later.

---
+++ Assets/Scripts/ItemTriggerCheck.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemTriggerCheck : MonoBehaviour
+{
+    //The objects the script is looking for. Need to be an array later for check of multiple items.
+    public GameObject interactableObject;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == interactableObject)
+            GetComponent<Collider>().enabled = false;
+    }
+
+    //Runs when item is placer. Need to discuss with the rest of the group on how to do this in a good way.
+    void ItemPlaced()
+    {
+    }
+}
---
+++ Assets/Scripts/StoryboardProgression.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StoryboardProgression : MonoBehaviour {
+
+    [SerializeField]string[] answers;
+    [SerializeField]Scene[] scenes;         //Need to find a way to assign values in the inspector
+
+    Rect rect;
+
+    void Start()
+    {
+        rect = new Rect(0 + 10, Screen.height - 100, Screen.width, Screen.height);
+    }
+
+    //Creates text which is entered in the inspector and displayed on the bottom left of the screen. Needs a minimum of 2 answers.
+    void OnGUI()
+    {
+        GUI.Label(new Rect(rect.x, rect.y - 60, rect.width, rect.height), "Pick one answer:");      //Need to make this editable in the inspector as well
+        GUI.Label(new Rect(rect.x, rect.y - 40, rect.width, rect.height), "\n1. " + answers[0]);
+        GUI.Label(new Rect(rect.x, rect.y - 20, rect.width, rect.height),"\n2. " + answers[1]);
+        if (answers.Length > 2)
+            GUI.Label(rect,"\n3. " + answers[2]);
+    }
+
+    //If the player presses a key from 1 to 3 the scene will change
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SceneManager.LoadScene(scenes[0].name);
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && scenes[1] != null)
+            SceneManager.LoadScene(scenes[1].name);
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && scenes[2] != null)
+            SceneManager.LoadScene(scenes[2].name);
+    }
+}
