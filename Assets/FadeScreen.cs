964e2d9a31870e201c92ba9a6b587474d65daf6d
Alexander Lövberg <lovbergalexander@gmail.com>
2016-11-29 12:44:00 +0100

    Backup

---
+++ Assets/FadeScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeScreen : MonoBehaviour {
+
+    public Image blackScreen;
+    public Color fadeToColor;
+
+    public float fadeTimeMultiplier;
+
+    void Start()
+    {
+        fadeToColor = Color.clear;
+    }
+
+    void FadeToBlack()
+    {
+        if(blackScreen.color.a > 0.05f)
+            blackScreen.color = Color.Lerp(blackScreen.color, fadeToColor, Time.deltaTime * fadeTimeMultiplier);
+    }
+
+    void Update()
+    {
+        FadeToBlack();
+    }
+}
