8856d224ef2f5ccec328efcf8cf601e41b71b8e2
Saxmax <max@maxhuber.se>
2016-11-30 18:22:37 +0100

    Added gamlingroom story
    Door gets shut when player approaches.

---
+++ Assets/Scripts/GamblingDoor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GamblingDoor : MonoBehaviour {
+
+	public GameObject doorToClose;
+	public AudioClip doorSlamClip;
+	public float doorRotationDegrees = 42f;
+
+	private AudioSource playerAudioSource;
+	private bool hasClosed = false;
+
+	void CloseDoor() {
+		if(hasClosed) return;
+
+		hasClosed = true;
+		doorToClose.transform.Rotate(Vector3.up * doorRotationDegrees, Space.World);
+		playerAudioSource.clip = doorSlamClip;
+		playerAudioSource.Play();
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(col.CompareTag("Player")) {
+			playerAudioSource = col.GetComponent<AudioSource>();
+			CloseDoor();
+		}
+	}
+
+}
