4507ee59b394a9b44bb4a3f105bf06d46fdfd8da
Alexander Lövberg <lovbergalexander@gmail.com>
2016-12-01 17:48:44 +0100

    Storyboard

--- Assets/Scripts/StoryboardProgression.cs
+++ Assets/Scripts/StoryboardProgression.cs
@@ -5,17 +5,15 @@ using System.Collections;
 
 public class StoryboardProgression : MonoBehaviour {
 
-    [SerializeField]string question;
     [SerializeField]string[] answers;
     [SerializeField]Object[] scenes;
 
     Rect rect;
     GUIStyle gStyle = new GUIStyle();
-    public Color fontColor;
+    //public Color fontColor;
 
     void Awake()
     {
-        Debug.Log(fontColor);
         rect = new Rect(0 + 10, Screen.height - 100, Screen.width, Screen.height);
 
     }
@@ -23,17 +21,20 @@ public class StoryboardProgression : MonoBehaviour {
     void Start()
     {
         gStyle.fontSize = 30;
-        gStyle.font.material.color = fontColor;
     }
 
     //Creates text which is entered in the inspector and displayed on the bottom left of the screen. Needs a minimum of 2 answers.
     void OnGUI()
     {
-        GUI.Label(new Rect(rect.x, rect.y - 90, rect.width, rect.height), question, gStyle);      //Need to make this editable in the inspector as well
-        GUI.Label(new Rect(rect.x, rect.y - 60, rect.width, rect.height), "\n1. " + answers[0], gStyle);
-        GUI.Label(new Rect(rect.x, rect.y - 30, rect.width, rect.height),"\n2. " + answers[1], gStyle);
+        GUI.Label(new Rect(rect.x, rect.y - 120, rect.width, rect.height), "\n1. " + answers[0], gStyle);
+        if (answers.Length > 1)
+            GUI.Label(new Rect(rect.x, rect.y - 90, rect.width, rect.height),"\n2. " + answers[1], gStyle);
         if (answers.Length > 2)
-            GUI.Label(rect,"\n3. " + answers[2], gStyle);
+            GUI.Label(new Rect(rect.x, rect.y - 60, rect.width, rect.height), "\n3. " + answers[2], gStyle);
+        if (answers.Length > 3)
+            GUI.Label(new Rect(rect.x, rect.y - 30, rect.width, rect.height), "\n4. " + answers[3], gStyle);
+        if (answers.Length > 4)
+            GUI.Label(new Rect(rect.x, rect.y, rect.width, rect.height), "\n5. " + answers[4], gStyle);
     }
 
     //If the player presses a key from 1 to 3 the scene will change
@@ -45,5 +46,9 @@ public class StoryboardProgression : MonoBehaviour {
             SceneManager.LoadScene(scenes[1].name);
         else if (Input.GetKeyDown(KeyCode.Alpha3) && scenes[2] != null)
             SceneManager.LoadScene(scenes[2].name);
+        else if (Input.GetKeyDown(KeyCode.Alpha4) && scenes[3] != null)
+            SceneManager.LoadScene(scenes[3].name);
+        else if (Input.GetKeyDown(KeyCode.Alpha5) && scenes[4] != null)
+            SceneManager.LoadScene(scenes[4].name);
     }
 }
