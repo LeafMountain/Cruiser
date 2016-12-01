88c9746ee68ace9a596bc97dedd3f704f8db95cd
Alexander Lövberg <lovbergalexander@gmail.com>
2016-12-01 13:37:49 +0100

    Backup b4 conflict

--- Assets/Scripts/StoryboardProgression.cs
+++ Assets/Scripts/StoryboardProgression.cs
@@ -5,24 +5,35 @@ using System.Collections;
 
 public class StoryboardProgression : MonoBehaviour {
 
+    [SerializeField]string question;
     [SerializeField]string[] answers;
-    [SerializeField]Scene[] scenes;         //Need to find a way to assign values in the inspector
+    [SerializeField]Object[] scenes;
 
     Rect rect;
+    GUIStyle gStyle = new GUIStyle();
+    public Color fontColor;
+
+    void Awake()
+    {
+        Debug.Log(fontColor);
+        rect = new Rect(0 + 10, Screen.height - 100, Screen.width, Screen.height);
+
+    }
 
     void Start()
     {
-        rect = new Rect(0 + 10, Screen.height - 100, Screen.width, Screen.height);
+        gStyle.fontSize = 30;
+        gStyle.font.material.color = fontColor;
     }
 
     //Creates text which is entered in the inspector and displayed on the bottom left of the screen. Needs a minimum of 2 answers.
     void OnGUI()
     {
-        GUI.Label(new Rect(rect.x, rect.y - 60, rect.width, rect.height), "Pick one answer:");      //Need to make this editable in the inspector as well
-        GUI.Label(new Rect(rect.x, rect.y - 40, rect.width, rect.height), "\n1. " + answers[0]);
-        GUI.Label(new Rect(rect.x, rect.y - 20, rect.width, rect.height),"\n2. " + answers[1]);
+        GUI.Label(new Rect(rect.x, rect.y - 90, rect.width, rect.height), question, gStyle);      //Need to make this editable in the inspector as well
+        GUI.Label(new Rect(rect.x, rect.y - 60, rect.width, rect.height), "\n1. " + answers[0], gStyle);
+        GUI.Label(new Rect(rect.x, rect.y - 30, rect.width, rect.height),"\n2. " + answers[1], gStyle);
         if (answers.Length > 2)
-            GUI.Label(rect,"\n3. " + answers[2]);
+            GUI.Label(rect,"\n3. " + answers[2], gStyle);
     }
 
     //If the player presses a key from 1 to 3 the scene will change
