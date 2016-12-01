ffe407dbb0a75b4009f24be93ac86b6aa285d844
Alexander Lövberg <lovbergalexander@gmail.com>
2016-12-01 12:18:30 +0100

    Scene change

---
+++ Assets/Scripts/DisableEnableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableEnableObject : MonoBehaviour {
+
+    public GameObject enableThis;
+    public GameObject disableThis;
+	
+	public void EnableDisable ()
+    {
+        enableThis.SetActive(true);
+        disableThis.SetActive(false);
+	}
+
+    void Start()
+    {
+        EnableDisable();
+    }
+}
