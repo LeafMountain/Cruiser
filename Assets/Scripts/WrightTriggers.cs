928b4b85a6272f617c877accb56f46d90478941a
Saxmax <max@maxhuber.se>
2016-12-02 16:31:27 +0100

    Final touches
    :)

---
+++ Assets/Scripts/WrightTriggers.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class WrightTriggers : MonoBehaviour {
+	void OnTriggerExit(Collider col) {
+		if(col.CompareTag("Player"))
+			UnityEngine.SceneManagement.SceneManager.LoadScene(24);
+	}
+}
