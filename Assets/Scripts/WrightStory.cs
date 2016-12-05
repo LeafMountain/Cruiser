7d4c5e9a2391f956efadee5358635c68aa95abaa
Saxmax <max@maxhuber.se>
2016-12-05 14:43:16 +0100

    FINAL BUILD
    YES

--- Assets/Scripts/WrightStory.cs
+++ Assets/Scripts/WrightStory.cs
@@ -6,11 +6,17 @@ public class WrightStory : MonoBehaviour {
 
 	public GameObject poison;
 	public GameObject water;
+	public AudioSource wrightSource;
 
 	void OnTriggerEnter(Collider col) {
-		if(col.gameObject == poison)
-			SceneManager.LoadScene(22);
-		else if(col.gameObject == water)
-			SceneManager.LoadScene(23);
+		if(col.gameObject == poison || col.gameObject == water) {
+			StartCoroutine(WrightThanks(col.gameObject == poison));
+		}
+	}
+
+	IEnumerator WrightThanks(bool poison) {
+		wrightSource.Play();
+		yield return new WaitForSeconds(wrightSource.clip.length);
+		SceneManager.LoadScene(poison ? 22 : 23);
 	}
 }
